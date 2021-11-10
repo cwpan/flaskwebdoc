e1f5603ecaf16649a81df9c5903f46d2dda151b5
William <cwpan@users.noreply.github.com>
2021-11-09 20:25:27 -0500

    Update app.py

--- app.py
+++ app.py
@@ -20,5 +20,5 @@ def predict():
     output= round(prediction[0], 2)
     return render_template("index.html", prediction_text= "flower is {}".format(output))
 #running the flask app
-if __name__= "__main__":
+if __name__== "__main__":
     app.run(host='127.0.0.1', port=8080, debug=True)
